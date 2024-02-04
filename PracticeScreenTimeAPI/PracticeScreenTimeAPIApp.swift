351372a4ac2407221556079891f09ecedfb463b7
0102johnny <rt2824.recruit.1022@gmail.com>
2024-02-04 17:18:26 +0900

    Initial commit

---
+++ PracticeScreenTimeAPI/PracticeScreenTimeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeScreenTimeAPIApp.swift
+//  PracticeScreenTimeAPI
+//
+//  Created by Johnny Toda on 2024/02/04.
+//
+
+import SwiftUI
+
+@main
+struct PracticeScreenTimeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
