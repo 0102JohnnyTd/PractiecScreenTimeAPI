0d38496f2095ead1dd9fd91ba880ea77298c8eaa
0102johnny <rt2824.recruit.1022@gmail.com>
2024-02-04 17:24:47 +0900

    [add] FamilyControlsの承認をリクエストするコードを追加

--- PracticeScreenTimeAPI/PracticeScreenTimeAPIApp.swift
+++ PracticeScreenTimeAPI/PracticeScreenTimeAPIApp.swift
@@ -6,12 +6,22 @@
 //
 
 import SwiftUI
+import FamilyControls
 
 @main
 struct PracticeScreenTimeAPIApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    Task {
+                        do {
+                            try await AuthorizationCenter.shared.requestAuthorization(for: .individual)
+                        } catch {
+                            print("FamilyControls承認エラーが発生:", error)
+                        }
+                    }
+                }
         }
     }
 }
