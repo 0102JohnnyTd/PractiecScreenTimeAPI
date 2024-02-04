f20c334ced5325401ae3f5034824c6f72a0407e5
0102johnny <rt2824.recruit.1022@gmail.com>
2024-02-04 18:16:38 +0900

    [add] FamilyActivityPickerを実装

--- PracticeScreenTimeAPI/ContentView.swift
+++ PracticeScreenTimeAPI/ContentView.swift
@@ -6,19 +6,43 @@
 //
 
 import SwiftUI
+import FamilyControls
 
 struct ContentView: View {
+    @State private var selection = FamilyActivitySelection()
+    @State private var isPresented = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button {
+            isPresented = true
+        } label: {
+            Text("選択する")
         }
-        .padding()
+        .familyActivityPicker(
+            isPresented: $isPresented,
+            selection: $selection
+        )
     }
 }
 
 #Preview {
     ContentView()
 }
+
+//struct ContentView: View {
+//
+//    @StateObject var viewModel: ContentViewModel
+//    @State private var isPresented = false
+//
+//    var body: some View {
+//        Button {
+//            isPresented = true
+//        } label: {
+//            Text("選択する")
+//        }
+//        .familyActivityPicker(
+//            isPresented: $isPresented,
+//            selection: $viewModel.selection
+//        )
+//    }
+//}
